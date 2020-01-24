383bc2f0a8845536440db34cd65a3d28eef8edf7
Axel Boersma <dana@compile.ictabc.net>
2020-01-24 16:18:43 +0000

    Added extra meters to P1 dongle

--- drivers/p1_dongle/device.js
+++ drivers/p1_dongle/device.js
@@ -12,7 +12,7 @@ onMeshInit() {
 		this.printNode();
 
 this.registerCapability('measure_power', 'METER');
-this.registerCapability('meter_power.normal', 'METER', {
+this.registerCapability('meter_power.kwh.normal', 'METER', {
 			multiChannelNodeId: 1,
 			pollInterval: 'poll_interval_channel_1',
 			get: 'METER_GET',
@@ -33,7 +33,70 @@ this.registerCapability('meter_power.normal', 'METER', {
 				return null;
 }
 });
-this.registerCapability('meter_power.low', 'METER', {
+this.registerCapability('meter_power.current_w.normal', 'METER', {
+			multiChannelNodeId: 1,
+			pollInterval: 'poll_interval_channel_1',
+			get: 'METER_GET',
+			getParser: () => ({
+				Properties1: {
+					'Rate Type': 'Import',
+					Scale: 2,
+				},
+				'Scale 2': 0,
+			}),
+			report: 'METER_REPORT',
+			reportParser: report => {
+				if (report.hasOwnProperty('Properties2')
+					&& report.Properties2.hasOwnProperty('Scale bits 10')
+					&& report.Properties2['Scale bits 10'] === 0) {
+					return report['Meter Value (Parsed)'];
+				}
+				return null;
+}
+});
+this.registerCapability('meter_power.kwh.produced.normal', 'METER', {
+			multiChannelNodeId: 1,
+			pollInterval: 'poll_interval_channel_1',
+			get: 'METER_GET',
+			getParser: () => ({
+				Properties1: {
+					'Rate Type': 'Export',
+					Scale: 0,
+				},
+				'Scale 2': 0,
+			}),
+			report: 'METER_REPORT',
+			reportParser: report => {
+				if (report.hasOwnProperty('Properties2')
+					&& report.Properties2.hasOwnProperty('Scale bits 10')
+					&& report.Properties2['Scale bits 10'] === 0) {
+					return report['Meter Value (Parsed)'];
+				}
+				return null;
+}
+});
+this.registerCapability('meter_power.current_w.produced.normal', 'METER', {
+			multiChannelNodeId: 1,
+			pollInterval: 'poll_interval_channel_1',
+			get: 'METER_GET',
+			getParser: () => ({
+				Properties1: {
+					'Rate Type': 'Export',
+					Scale: 2,
+				},
+				'Scale 2': 0,
+			}),
+			report: 'METER_REPORT',
+			reportParser: report => {
+				if (report.hasOwnProperty('Properties2')
+					&& report.Properties2.hasOwnProperty('Scale bits 10')
+					&& report.Properties2['Scale bits 10'] === 0) {
+					return report['Meter Value (Parsed)'];
+				}
+				return null;
+}
+});
+this.registerCapability('meter_power.kwh.low', 'METER', {
 			multiChannelNodeId: 2,
 			pollInterval: 'poll_interval_channel_2',
 			get: 'METER_GET',
@@ -41,6 +104,69 @@ this.registerCapability('meter_power.low', 'METER', {
 				Properties1: {
 					'Rate Type': 'Import',
 					Scale: 0,
+				},
+				'Scale 2': 0,
+			}),
+			report: 'METER_REPORT',
+			reportParser: report => {
+				if (report.hasOwnProperty('Properties2')
+					&& report.Properties2.hasOwnProperty('Scale bits 10')
+					&& report.Properties2['Scale bits 10'] === 0) {
+					return report['Meter Value (Parsed)'];
+				}
+				return null;
+}
+});
+this.registerCapability('meter_power.w_current.low', 'METER', {
+			multiChannelNodeId: 2,
+			pollInterval: 'poll_interval_channel_2',
+			get: 'METER_GET',
+			getParser: () => ({
+				Properties1: {
+					'Rate Type': 'Import',
+					Scale: 2,
+				},
+				'Scale 2': 0,
+			}),
+			report: 'METER_REPORT',
+			reportParser: report => {
+				if (report.hasOwnProperty('Properties2')
+					&& report.Properties2.hasOwnProperty('Scale bits 10')
+					&& report.Properties2['Scale bits 10'] === 0) {
+					return report['Meter Value (Parsed)'];
+				}
+				return null;
+}
+});
+this.registerCapability('meter_power.kwh.produced.low', 'METER', {
+			multiChannelNodeId: 2,
+			pollInterval: 'poll_interval_channel_2',
+			get: 'METER_GET',
+			getParser: () => ({
+				Properties1: {
+					'Rate Type': 'Export',
+					Scale: 0,
+				},
+				'Scale 2': 0,
+			}),
+			report: 'METER_REPORT',
+			reportParser: report => {
+				if (report.hasOwnProperty('Properties2')
+					&& report.Properties2.hasOwnProperty('Scale bits 10')
+					&& report.Properties2['Scale bits 10'] === 0) {
+					return report['Meter Value (Parsed)'];
+				}
+				return null;
+}
+});
+this.registerCapability('meter_power.w_current.produced.low', 'METER', {
+			multiChannelNodeId: 2,
+			pollInterval: 'poll_interval_channel_2',
+			get: 'METER_GET',
+			getParser: () => ({
+				Properties1: {
+					'Rate Type': 'Export',
+					Scale: 2,
 				},
 				'Scale 2': 0,
 			}),
